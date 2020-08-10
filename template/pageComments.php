ebb76919c1d61f08a60c1aeb17a74fb885f9f5a5
Mikhail Gusev <king-tut@yandex.ru>
2020-08-10 23:43:04 +0300

    Немного адаптива

--- template/pageComments.php
+++ template/pageComments.php
@@ -6,9 +6,9 @@
             <form action="/" method="POST">
                 <h2>Оставить комментарий</h2>
                 <label for="name">Ваше имя</label>
-                <input id="name" type="text" name="name">
+                <input id="name" type="text" name="name" required>
                 <label for="comment">Ваш комментарий</label>
-                <textarea name="comment" id="comment"></textarea>
+                <textarea name="comment" id="comment" required></textarea>
                 <input type="submit" name="send" value="Отправить">
             </form>
         </div>
